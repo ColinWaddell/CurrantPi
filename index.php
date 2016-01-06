8fe844669c583012e5fa229c40f772974939f597
Colin Waddell <mrcolin@gmail.com>
2016-01-06 20:16:47 +0000

    added in memory info and some scroll bars

--- index.php
+++ index.php
@@ -34,8 +34,7 @@
     <title>Raspberry Pi - Board Details</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
+  <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- Custom styles for this template -->
     <link href="css/custom.css" rel="stylesheet">
   </head>
@@ -57,15 +56,15 @@
             $rates = explode(' ', $output);
            ?>
 
-          <table class="table table-striped">
+          <table class="table table-striped table-hover">
             <tbody>
             <tr>
-              <td>Down:</td>
-              <td><?php echo pretty_baud($rates[0]);?></td>
+              <td><p>Down:</p></td>
+              <td><p class="text-right"><?php echo pretty_baud($rates[0]);?></p></td>
             </tr>
             <tr>
-              <td>Up:</td>
-              <td><?php echo pretty_baud($rates[1]);?></td>
+              <td><p>Up:</p></td>
+              <td><p class="text-right"><?php echo pretty_baud($rates[1]);?></p></td>
             </tr>
             </tbody>
           </table>
@@ -82,15 +81,101 @@
             $time_alive = seconds_to_time(intval($output));
           ?>
 
-          <table class="table table-striped">
+          <table class="table table-striped table-hover">
             <tbody>
             <tr>
-              <td>Time Alive: </td>
-              <td><?php echo "$time_alive";?></td>
+              <td><p>Time Alive: </p></td>
+              <td><p class="text-right"><?php echo "$time_alive";?></p></td>
             </tr>
             <tr>
-              <td>Board Temperature: </td>
-              <td><?php echo "$temp&deg;C";?></td>
+              <td><p>Board Temperature: </p></td>
+              <td><p class="text-right"><?php echo "$temp&deg;C";?></p></td>
+            </tr>
+           </tbody>
+          </table>
+        </div>
+      </div>
+
+
+      <div class="row">
+        <div class="col-lg-6">
+         <h4>Memory</h4>
+
+          <?php
+            $output = shell_exec('cat /proc/meminfo');
+            $table_rows = preg_split ('/$\R?^/m', $output);
+            $row_mem_total = $table_rows[0];
+            $row_mem_free  = $table_rows[1];
+
+            $mem_total = explode(' ', $row_mem_total);
+            $mem_total = $mem_total[count($mem_total)-2];
+
+            $mem_free = explode(' ', $row_mem_free);
+            $mem_free = $mem_free[count($mem_free)-2];
+
+            $mem_used_percentage = floor((($mem_total-$mem_free)/$mem_total)*100);
+          ?>
+
+          <table class="table table-striped table-hover">
+            <tbody>
+            <tr>
+              <td>
+                <p>Free:</p>
+              </td>
+              <td>
+               <p class="text-right"><?php echo floor($mem_free/1024); ?>MB&nbsp;</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>Total:</p>
+              </td>
+              <td>
+               <p class="text-right"><?php echo floor($mem_total/1000); ?>MB</p>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2">
+                <div class="progress">
+                  <div
+                    class="progress-bar" 
+                    role="progressbar" 
+                    aria-valuenow="<?php echo $mem_used_percentage; ?>" 
+                    aria-valuemin="0" 
+                    aria-valuemax="100" 
+                    style="min-width: 2em; width: <?php echo $mem_used_percentage; ?>%;"
+                  >
+                    <?php echo $mem_used_percentage; ?>%
+                  </div>
+                </div>
+              </td>
+            </tr>
+           </tbody>
+          </table>
+        </div>
+
+        <div class="col-lg-6">
+         <h4>Processor</h4>
+
+          <?php
+            $output = shell_exec('cat /proc/cpuinfo');
+            $table_rows = preg_split ('/$\R?^/m', $output);
+            // string trim ( string $str [, string $character_mask = " \t\n\r\0\x0B" ] )
+          ?>
+
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <?php
+
+                ?>
+              </tr>
+            </thead>
+            <tbody>
+            <tr>
+               <?php
+
+              ?>           
             </tr>
            </tbody>
           </table>
@@ -109,13 +194,26 @@
             $table_header = array_splice($table_header, 0, count($table_header)-1);
           ?>
 
-          <table class="table table-striped">
+          <table class="table table-striped table-hover">
             <thead>
               <tr>
                 <?php
+                  $col_count = 0;
                   foreach($table_header as $header) {
                     if ($header!==""){
-                      echo "<th>$header</th>";
+                      if($col_count==4){
+                        echo "<th><p style='min-width: 6em;' class='text-center'>$header</p></th>";
+                      }
+                      else if($col_count==5){
+                        echo "<th><p class='text-right'>$header</p></th>";
+                      }
+                      else if($col_count>0){
+                        echo "<th><p class='text-center'>$header</p></th>";
+                      }
+                      else{
+                        echo "<th><p>$header</p></th>";
+                      }
+                      $col_count++;
                     }
                   }
                 ?>
@@ -127,9 +225,38 @@
                 foreach($table_rows as $row) {
                   echo "<tr>";
                   $items = explode(' ', $row);
+                  $col_count = 0;
                   foreach($items as $item){
                     if ($item!==""){
-                      echo "<td>$item</td>";
+                      if($col_count==4){ 
+                        $percentage = intval($item);
+                        ?>
+
+                        <td>
+                          <div class="progress">
+                            <div
+                              class="progress-bar" 
+                              role="progressbar" 
+                              aria-valuenow="<?php echo $percentage; ?>" 
+                              aria-valuemin="0" 
+                              aria-valuemax="100" 
+                              style="width: <?php echo $percentage; ?>%;"
+                            >
+                              <?php echo $percentage; ?>%
+                            </div>
+                          </div>
+                        </td>
+                      <?php }
+                      else if($col_count==5){
+                        echo "<td><p class='text-right'>$item</p></td>";
+                      }
+                      else if($col_count>0){
+                        echo "<td><p class='text-center'>$item</p></td>";
+                      }
+                      else{
+                        echo "<td><p>$item</p></td>";
+                      }
+                      $col_count++;
                     }
                   }
                   echo "</tr>";
