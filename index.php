e8d80ef79f71c31a18faba85fa87d10c57e3a6fd
Guilhem Eydieux <mephissto@gmail.com>
2016-01-07 22:09:07 +0100

    Add space between minutes

--- index.php
+++ index.php
@@ -73,15 +73,15 @@
           <table class="table table-striped table-hover">
             <tbody>
             <tr>
-              <td><p>1min:</p></td>
+              <td><p>1 min:</p></td>
               <td><p class="text-right"><?php echo substr($loadavg[0], 0, -1);?></p></td>
             </tr>
             <tr>
-              <td><p>5min:</p></td>
+              <td><p>5 min:</p></td>
               <td><p class="text-right"><?php echo substr($loadavg[1], 0, -1);?></p></td>
             </tr>
             <tr>
-              <td><p>15min:</p></td>
+              <td><p>15 min:</p></td>
               <td><p class="text-right"><?php echo $loadavg[2];?></p></td>
             </tr>
             </tbody>
