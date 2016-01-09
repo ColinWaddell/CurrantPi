f98853aa9ccead61fa53babb8d6a74288fc89cd3
Colin Waddell <mrcolin@gmail.com>
2016-01-09 17:00:27 +0000

    added license details to footer

--- content/footer.php
+++ content/footer.php
@@ -10,4 +10,5 @@
     echo $name[1];
   ?>
 </p>
+<p><a href="https://opensource.org/licenses/MIT">The MIT License (MIT)</a>
 <p><a href="https://github.com/ColinWaddell/RPi-Board-Info"><i class="fa fa-github-alt"></i> Source</a></p>
