86567b74e16277657a432141994cda71002ef19d
Guilhem Eydieux <Guilhem Eydieux>
2016-01-09 12:16:43 +0100

    Removed memory_bar.php & tweaked / fixed memory display

--- content/memory.php
+++ content/memory.php
@@ -1,8 +1,19 @@
 <h4><i class="fa fa-bolt"></i> Memory</h4>
 
 <?php
-  $mem_free = intval(shell_exec("free -m | awk '/buffers\/cache/ {print $3}'"));
-  $mem_total = intval(shell_exec("free -m | awk '/Mem/ {print $2}'"));
+  // RAM in MB
+  $total_m = intval(shell_exec("free -m | awk '/Mem/ {print $2}'"));
+  $used_m = intval(shell_exec("free -m | awk '/Mem/ {print $3}'"));
+  $free_m = intval(shell_exec("free -m | awk '/Mem/ {print $4}'"));
+
+  // RAM in bytes
+  $total = intval(shell_exec("free | awk '/Mem/ {print $2}'"));
+  $used = intval(shell_exec("free | awk '/Mem/ {print $3}'"));
+  $free = intval(shell_exec("free | awk '/Mem/ {print $4}'"));
+
+  // Percentage
+  $free_p = round(100*($free/$total), 2);
+  $used_p = round(100*($used/$total), 2);
 ?>
 
 <table class="table table-striped table-hover">
@@ -12,20 +23,23 @@
       <p>Free:</p>
     </td>
     <td>
-     <p class="text-right"><?php echo $mem_free; ?>&nbsp;MB</p>
+     <p class="text-right"><?php echo $free_m; ?>/<?php echo $total_m; ?>&nbsp;MB</p>
     </td>
   </tr>
   <tr>
     <td>
-      <p>Total:</p>
+      <p>Used:</p>
     </td>
     <td>
-     <p class="text-right"><?php echo $mem_total; ?>&nbsp;MB</p>
+     <p class="text-right"><?php echo $used_m; ?>/<?php echo $total_m; ?>&nbsp;MB</p>
     </td>
   </tr>
   <tr>
     <td colspan="2">
-      <?php include('memory_bar.php'); ?>
+      <div class="progress">
+        <div class="progress-bar progress-bar-free" style="width: <?php echo $free_p;?>%"><?php echo $free_p;?>%</div>  
+        <div class="progress-bar progress-bar-used" style="width: <?php echo $used_p;?>%"><?php echo $used_p;?>%</div>  
+      </div>
     </td>
   </tr>
  </tbody>
