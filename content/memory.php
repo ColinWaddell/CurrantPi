ef99351893632dc5a1f532ec297241c77d2787e4
Colin Waddell <mrcolin@gmail.com>
2016-01-13 20:49:44 +0000

    that made it too tricky to read

--- content/memory.php
+++ content/memory.php
@@ -70,7 +70,7 @@
         </div>
       </td>
       <td>
-        <p class="text-right">Used</p>
+        <p>Used</p>
       </td>
       <td class="membar-key">
         <span class="membar-key-buffers">
@@ -78,7 +78,7 @@
         </div>
       </td>
       <td>
-        <p class="text-right">Buffers</p>
+        <p>Buffers</p>
       </td>
     </tr>
 
@@ -89,7 +89,7 @@
         </div>
       </td>
       <td>
-        <p class="text-right">Cache</p>
+        <p>Cache</p>
       </td>
       <td class="membar-key">
         <span class="membar-key-free">
@@ -97,7 +97,7 @@
         </div>
       </td>
       <td>
-        <p class="text-right">Free</p>
+        <p>Free</p>
       </td>
     </tr> 
   </tbody>
