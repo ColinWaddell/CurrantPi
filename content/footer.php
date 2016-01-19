7500d6a085fe1d839924fe36e36c63d0a3867c49
Colin Waddell <mrcolin@gmail.com>
2016-01-19 18:41:43 +0000

    fixing spacing in license link

--- content/footer.php
+++ content/footer.php
@@ -13,4 +13,4 @@
 <p><a href="https://github.com/ColinWaddell/CurrantPi"><i class="demo-icon icon-github"></i></a>
 <a href="https://github.com/ColinWaddell/CurrantPi">Source</a></p>
 <hr />
-<p>&copy; 2016 <a href="http://colinwaddell.com/">Colin Waddell</a> under the terms of the<a href="LICENSE.txt"> MIT License.</a>
+<p>&copy; 2016 <a href="http://colinwaddell.com/">Colin Waddell</a> under the terms of the <a href="LICENSE.txt">MIT License</a></p>
