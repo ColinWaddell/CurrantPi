b59d025fed563138f6130375cc2d9034e72c19b0
Colin Waddell <mrcolin@gmail.com>
2016-01-19 18:42:16 +0000

    using custom css rather than muted

--- content/banner.php
+++ content/banner.php
@@ -1,6 +1,6 @@
 <div class="col-sm-8 title-text">
   <h1>Currant Pi</h1>
-  <h3 class="text-muted">Raspberry Pi Status</h3>
+  <h3>Raspberry Pi Status</h3>
 </div>
 <div class="col-sm-4 title-logo">
   <img src="img/Raspberry_Pi_Logo.svg" class="title_logo">
