0834f38b1b97040095a596b6ded9cff8dbfde83e
Colin Waddell <mrcolin@gmail.com>
2016-02-20 15:41:23 +0000

    add some details about CurrantModule and what it's for

--- lib/CurrantModule.php
+++ lib/CurrantModule.php
@@ -2,7 +2,17 @@
 
 namespace CurrantPi;
 
+/*
+ * The interface CurrantModule is used throughout
+ * the application by each content source to store
+ * and return the data relevant to the module.
+ */
+
 interface CurrantModule
 {
+    /*
+     * Returns an Object which can be
+     * used by the view and the api.
+     */
     public function getData();
 }
