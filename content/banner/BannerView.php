4714c5a5d10af1c7d407a5b8a1eb71b0aa9d6e4a
Knut Kohl <github@knutkohl.de>
2016-05-18 10:52:24 +0200

    Add dynamic hostname in Header view

--- content/banner/BannerView.php
+++ content/banner/BannerView.php
@@ -2,11 +2,13 @@
   /*
    * Title area. Check out custom.css for how this is tweaked. 
    */
+
+  $hostname = shell_exec('hostname');
 ?>
 
 <div class="col-sm-8 title-text">
   <h1>Currant Pi</h1>
-  <h3>Raspberry Pi Status</h3>
+  <h3><span id="hostname"><?php echo $hostname; ?></span> Status</h3>
 </div>
 <div class="col-sm-4 title-logo">
   <img src="img/Raspberry_Pi_Logo.svg" alt="Currant Pi Logo" class="title_logo">
