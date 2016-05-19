2d1b795aa5a9ed6d46651d60529a4e879f84ee4c
Colin Waddell <mrcolin@gmail.com>
2016-05-19 22:58:58 +0100

    tweaked the banner area to also include the hostname. Needs work

--- content/banner/BannerView.php
+++ content/banner/BannerView.php
@@ -8,8 +8,9 @@
 
 <div class="col-sm-8 title-text">
   <h1>Currant Pi</h1>
-  <h3><span id="hostname"><?php echo $hostname; ?></span> Status</h3>
+  <h3>Raspberry Pi Status</h3>
 </div>
 <div class="col-sm-4 title-logo">
   <img src="img/Raspberry_Pi_Logo.svg" alt="Currant Pi Logo" class="title_logo">
+  <h4><?php echo $hostname; ?></h4>
 </div>
