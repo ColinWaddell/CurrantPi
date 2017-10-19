fa29528e81a6555d5c2913bababf18d434df2807
Colin Waddell <mrcolin@gmail.com>
2017-10-19 19:26:07 +0100

    better placement on mobile

--- content/banner/BannerView.php
+++ content/banner/BannerView.php
@@ -6,11 +6,11 @@
   $hostname = shell_exec('hostname');
 ?>
 
-<div class="col-xs-8 title-text">
+<div class="col-sm-8 title-text">
   <h1>Currant Pi</h1>
   <h3>Raspberry Pi Status</h3>
 </div>
-<div class="col-xs-4 title-brand">
+<div class="col-sm-4 title-brand">
   <a class="title-logo">
     <div class="title-image">
       <?php echo $hostname; ?>
