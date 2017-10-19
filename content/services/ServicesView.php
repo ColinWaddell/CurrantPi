21a83b1bbc21a2e2f251977bf0e93d9fe12191c4
Colin Waddell <mrcolin@gmail.com>
2017-10-19 21:18:54 +0100

    displaying using labels

--- content/services/ServicesView.php
+++ content/services/ServicesView.php
@@ -1,10 +1,18 @@
-<?php
-namespace CurrantPi;
+<?php namespace CurrantPi; ?>
 
+<h4><i class="demo-icon icon-sliders"></i> Memory</h4>
+
+<?php
 include 'content/services/ServicesData.php';
 $services = new ServicesData();
 
-print_r( $services);
-
+foreach ($services->getData() as $service){
+  if($service->status=="active"){
+    echo '<span class="label label-success">'.$service->name.'</span> ';
+  }
+  else {
+    echo '<span class="label label-danger">'.$service->name.'</span> ';
+  }
+}
 ?>
 
